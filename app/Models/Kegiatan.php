d600c3b4fbc3d7881ae120819b3e7021dc76c6a0
rwaone <rwaone@gmail.com>
2023-03-01 15:17:00 +0800

    Update Kegiatan.php

--- app/Models/Kegiatan.php
+++ app/Models/Kegiatan.php
@@ -84,7 +84,7 @@ class Kegiatan extends Model
                     ->where('pegawai_id',$pegawai_id)
                     ->whereMonth('tanggal', $bulan)
                     ->whereYear('tanggal', $tahun)
-                    ->get();
+                    ->get('flag');
         return $flag;
     }
 }
