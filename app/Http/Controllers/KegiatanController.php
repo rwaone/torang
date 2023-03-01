598672b8817151a0110ef40f5b873e83b2b42ec8
rwaone <rwaone@gmail.com>
2023-03-01 08:55:44 +0800

    Update KegiatanController.php

--- app/Http/Controllers/KegiatanController.php
+++ app/Http/Controllers/KegiatanController.php
@@ -261,7 +261,7 @@ class KegiatanController extends Controller
             $pegawais = Pegawai::all();
         }else {
             $satker = Satker::where('id',auth()->user()->pegawai->satker_id)->get();
-            $pegawais = Pegawai::where('atasan_id', auth()->user()->pegawai->id);
+            $pegawais = Pegawai::where('penilai_id', auth()->user()->pegawai->id);
         }
         // $anggota_id = Timkerja::getMyAnggotaId();
         return view('pages.kegiatan.daftarpegawai', [
